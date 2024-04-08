ed9bb6f5820754b195689a9f31b5731086f78f5a
Welington Dias <33883532+Welin7@users.noreply.github.com>
2024-04-08 19:41:28 -0300

    Create project web api with generic controller.
    Create product models. Add Context for the Product. Create generic controller where the class ProductsController extend the class generics. This possible that the class ProductsController stay more clean.

---
+++ WebApiDynamics/WebApi/Controllers/DynamicController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DynamicController<T> : ControllerBase where T : class
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public DynamicController(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<T>>> GetAll()
+    {
+        return await _applicationDbContext.Set<T>().ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<T>> GetById(int id)
+    {
+        var entity = await _applicationDbContext.Set<T>().FindAsync(id);
+
+        if (entity == null) 
+        {
+            return NotFound();
+        }
+
+        return entity;        
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<T>> Post(T entity)
+    {
+        _applicationDbContext.Set<T>().Add(entity);
+        await _applicationDbContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetById), new { id = entity.GetType().GetProperty("id").GetValue(entity)}, entity);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<T>> Put(int id, T entity)
+    {
+        if (id != (int)entity.GetType().GetProperty("id").GetValue(entity))
+        {
+            return BadRequest();
+        }
+        
+        _applicationDbContext.Entry(entity).State = EntityState.Modified;
+        await _applicationDbContext.SaveChangesAsync();
+        return NoContent();           
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<T>> Delete(int id)
+    {
+        var entity = await _applicationDbContext.Set<T>().FindAsync(id);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+        
+        _applicationDbContext.Set<T>().Remove(entity);
+        await _applicationDbContext.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ WebApiDynamics/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : DynamicController<Product>
+{
+    public ProductsController(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+    {
+
+    }
+}
